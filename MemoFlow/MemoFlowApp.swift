fab003aad2f4d87356c507f085ed0ab49c901254
kawaichang8 <info@shunyakawai.com>
2025-12-27 09:38:53 +0900

    Initial Commit

---
+++ MemoFlow/MemoFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoFlowApp.swift
+//  MemoFlow
+//
+//  Created by Shunya Kawai on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct MemoFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
